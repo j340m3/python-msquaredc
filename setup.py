089f83b8d749f341da8ae25c87b33b76bd150047
j340m3 <bergmann.jerome@gmail.com>
2018-04-06 15:03:17 +0200

    Fixing docs... again.

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ def read(*names, **kwargs):
         encoding=kwargs.get('encoding', 'utf8')
     ).read()
 
-#subprocess.check_output(['git', 'show', '-s', '--format=%ct','HEAD^{commit}']).decode('latin-1').strip()
+# subprocess.check_output(['git', 'show', '-s', '--format=%ct','HEAD^{commit}']).decode('latin-1').strip()
+
 
 buildnr = "."
 try:
